06bfa27813157fa120bdb53cdfae2781585d8715
Ariadna Villagra <ariadna.sofia.villagra@gmail.com>
2024-08-31 19:30:42 -0300

    primer ej

---
+++ poo.py
@@ -0,0 +1,20 @@
+#-------------------------------------------------------------------------------
+# Name:        Clase 1
+# Author:      Ariadna Villagra
+
+#1. Definir dos variables de distinto valor numérico que ejecuten una operación y finalmente se
+#muestren por pantalla.
+#2. Definir dos variables con diferentes cadenas de texto que se concatenen y finalmente se
+#muestren por pantalla.
+#3. Solicitar que el usuario ingrese Nombre y DNI, guardarlo en distintas variables e imprimir por
+#pantalla “El nombre del usuario es XXXXXX y su DNI es YYYYYY”.
+#4. Utilizar uno o más comentarios que indique lo que hace el código
+#5. Cada punto debe estar en un commit diferente subido al repositorio remoto.
+#-------------------------------------------------------------------------------
+#Ejercicio 1
+#Crea variables e imprime la suma de estas variables
+numero1 = 1
+numero2 = 2
+print (numero1 + numero2)
+
+
